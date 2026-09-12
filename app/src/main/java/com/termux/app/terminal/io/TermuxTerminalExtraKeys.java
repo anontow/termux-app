aee5af7acf09f3158cd36a61b20480cf39487829
anontow <63472879+anontow@users.noreply.github.com>
2026-09-12 19:11:47 +0600

    Update TermuxTerminalExtraKeys.java

--- app/src/main/java/com/termux/app/terminal/io/TermuxTerminalExtraKeys.java
+++ app/src/main/java/com/termux/app/terminal/io/TermuxTerminalExtraKeys.java
@@ -104,8 +104,3 @@ public class TermuxTerminalExtraKeys extends TerminalExtraKeys {
         }
     }
 }
-        } else {
-            super.onTerminalExtraKeyButtonClick(view, key, ctrlDown, altDown, shiftDown, fnDown);
-        }
-}
-
